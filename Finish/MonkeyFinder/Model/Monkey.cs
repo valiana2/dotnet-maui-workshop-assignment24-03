6139ba67cc4f18fd5fe724aa3369c2e28a39b68f
valiana2 <96139371+valiana2@users.noreply.github.com>
2023-03-24 20:41:16 +0700

    Update Monkey.cs

--- Finish/MonkeyFinder/Model/Monkey.cs
+++ Finish/MonkeyFinder/Model/Monkey.cs
@@ -2,11 +2,10 @@
 
 public class Monkey
 {
-    public string Name { get; set; }
-    public string Location { get; set; }
-    public string Details { get; set; }
-    public string Image { get; set; }
-    public int Population { get; set; }
-    public double Latitude { get; set; }
-    public double Longitude { get; set; }
+    public string Date{ get; set; }
+    public string Description { get; set; }
+    public string Lang { get; set; }
+    public string Category1 { get; set; }
+    public string Category2 { get; set; }
+    public string Granularity { get; set; }
 }
