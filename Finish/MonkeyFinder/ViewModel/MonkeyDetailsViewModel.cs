8b0b7850a0abbadc798043785175024625f8449f
valiana2 <96139371+valiana2@users.noreply.github.com>
2023-03-25 14:55:05 +0700

    Update MonkeyDetailsViewModel.cs

--- Finish/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ Finish/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -3,30 +3,7 @@
 [QueryProperty(nameof(Monkey), "Monkey")]
 public partial class MonkeyDetailsViewModel : BaseViewModel
 {
-    IMap map;
-    public MonkeyDetailsViewModel(IMap map)
-    {
-        this.map = map;
-    }
 
     [ObservableProperty]
     Monkey monkey;
-
-    [RelayCommand]
-    async Task OpenMap()
-    {
-        try
-        {
-            await map.OpenAsync(Monkey.Latitude, Monkey.Longitude, new MapLaunchOptions
-            {
-                Name = Monkey.Name,
-                NavigationMode = NavigationMode.None
-            });
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine($"Unable to launch maps: {ex.Message}");
-            await Shell.Current.DisplayAlert("Error, no Maps app!", ex.Message, "OK");
-        }
-    }
 }
